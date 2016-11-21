9913bd65e465587e33e6ce9dbc8142b6bfdac6b4
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-11-21 14:49:38 -0200

    Added notification handler

--- UsingNotifications/ViewController.swift
+++ UsingNotifications/ViewController.swift
@@ -23,8 +23,9 @@ class ViewController: UIViewController {
                 
             } else{
                 
-                print(error?.localizedDescription)
-                
+                if let err = error?.localizedDescription{
+                    print(err)
+                }
             }
             
         })
@@ -32,7 +33,43 @@ class ViewController: UIViewController {
     
     @IBAction func getNotificationPressed(sender: UIButton){
         
+        scheduleNotification(inSeconds: 5, completion: { success in
         
+            if success{
+                print("Done")
+            } else{
+                print("Error")
+            }
+        
+        })
+        
+    }
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()){
+        
+        let notif = UNMutableNotificationContent()
+        notif.title = "New notification"
+        notif.subtitle = "Realy nice!!!"
+        notif.body = "Using notification on iOS 10 is just amazing!!!"
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+        
+            if error != nil{
+                
+                print(error!)
+                completion(false)
+                
+            } else{
+                
+                completion(true)
+                
+            }
+        
+        })
         
     }
 
