59f435730a9d0e5085058e2996ff1e13b046f1d9
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-11-21 13:48:30 -0200

    Initial Commit

---
+++ UsingNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingNotifications
+//
+//  Created by Brunno Goncalves on 21/11/16.
+//  Copyright © 2016 brunnogoncalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
